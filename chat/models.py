db7585999e275b1b5b68440a7da7bc0c68084698
vimal-11 <vimalaadith.bs@gmail.com>
2021-08-31 10:19:35 +0530

    chat application implemented - initial code

---
+++ chat/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.fields import related
+
+User = get_user_model()
+
+class Message(models.Model):
+    author = models.ForeignKey(User, related_name='message_author', on_delete=models.CASCADE)
+    msg_content = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    def last_20_messages():
+        return Message.objects.order_by('-time').all()[:20]
