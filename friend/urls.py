a2ab1aae6da40e76605d040dd6b05e4c8c1dc7da
vimal-11 <vimalaadith.bs@gmail.com>
2021-12-26 17:33:38 +0530

    cancel request feature added

--- friend/urls.py
+++ friend/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('friend_request_accept/<friend_request_id>/', views.accept_friend_request, name='friend-request-accept'),
     path('friend_remove/', views.remove_friend, name='remove-friend'),
     path('friend_request_decline/<friend_request_id>/', views.decline_friend_request, name='friend-request-decline'),
+    path('friend_request_cancel/', views.cancel_friend_request, name='friend-request-cancel'),
 ]
