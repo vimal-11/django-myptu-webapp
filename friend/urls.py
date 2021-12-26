ac02d571129f52929377c1a985648fb494c369c5
vimal-11 <vimalaadith.bs@gmail.com>
2021-12-26 14:15:54 +0530

    Accepting friend requests
    1. url for accept_friend_request
    2. accept_friend_request view for calling ajax funtion.
    3. added base template for profile page in authenticate.

--- friend/urls.py
+++ friend/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 	
     path('friend_request/', views.send_friend_request, name='friend-request'),
     path('friend_requests/<user_id>/', views.friend_requests, name='friend-requests'),
+    path('friend_request_accept/<friend_request_id>/', views.accept_friend_request, name='friend-request-accept'),
 ]
