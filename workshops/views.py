38f885d6c0847f7b839de4d1ab468fe180f3542b
Hemachandran0x1 <phemachandran173@gmail.com>
2022-01-30 12:45:07 +0530

    Changes in workshops/views and templates.

--- workshops/views.py
+++ workshops/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http.response import HttpResponse
-import workshops
+from django.core.paginator import Paginator
 from .models import Workshop,Registered_workshop
+
 # Create your views here.
 
 def ListView(request):
-    workshop = get_object_or_404(Workshop,id=1)
-    context = {}
-    context["list"] = Workshop.objects.all()
-    return render(request,'workshops/list_workshop.html',context)
+    #workshop = get_object_or_404(Workshop,id=1)
+    workshop_list = Workshop.objects.all()
+    paginator = Paginator(workshop_list, 20)
+    page = request.GET.get('page')
+    #page = int(page)
+    #print(type(page))
+    #print(page)
+    page_obj = Paginator.get_page(paginator,page)
+    return render(request,'workshops/list_workshop.html',{'page_obj':page_obj})
 
     
