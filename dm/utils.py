1dea5b7df01b4e73c2da5c8fd900dbf912759662
vimal-11 <vimalaadith.bs@gmail.com>
2022-02-01 12:41:04 +0530

    consumers for dm

--- dm/utils.py
+++ dm/utils.py
@@ -48,6 +48,6 @@ class LazyRoomChatMessageEncoder(Serializer):
         dump_object.update({'user_id': str(obj.user.id)})
         dump_object.update({'username': str(obj.user.username)})
         dump_object.update({'message': str(obj.content)})
-        #dump_object.update({'profile_image': str(obj.user.profile_image.url)})
+        dump_object.update({'profile_image': str(obj.user.profile_image.url)})
         dump_object.update({'natural_timestamp': calculate_timestamp(obj.timestamp)})
         return 
