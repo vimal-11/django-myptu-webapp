8613bb565d0947e90d86b24eb3275b585fe0a227
Moginder <mogiegan@gmail.com>
2022-02-05 19:05:06 +0530

    Add files via upload

---
+++ feedsapi/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from feeds.models import Feeds, Comments
+
+
+class PostForm(forms.ModelForm):
+    body = forms.CharField(
+        label='',
+        widget=forms.Textarea(
+            attrs={'rows': '3',
+                   'placeholder': 'Say Something...'}
+        ))
+    image = forms.ImageField(
+        required=False,
+        widget=forms.ClearableFileInput(attrs={
+            'multiple': True
+            })
+    )
+
+    class Meta:
+        model = Feeds
+        fields = ['body', 'image', 'tags']
+        
+
+class CommentForm(forms.ModelForm):
+    comment = forms.CharField(
+        label='',
+        widget=forms.Textarea(
+            attrs={'rows': '3',
+                   'placeholder': 'Say Something...'}
+        ))
+
+    class Meta:
+        model = Comments
+        fields = ['comment']
