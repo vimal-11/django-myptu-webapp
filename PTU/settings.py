e01f1f3f45ad4bc6dec5df9b83978bd9d7b1ca58
vimal-11 <vimalaadith.bs@gmail.com>
2022-07-20 12:20:33 +0530

    tweaking settings

--- PTU/settings.py
+++ PTU/settings.py
@@ -28,7 +28,7 @@ EMAIL_PORT = EMAIL_PORT
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p1_$j5ee4@i_ly@1zi3ifpn*9$y79!16_7@7(f72p^u087_+)x'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
